7dd0e0d7e7fb48cd544cb44eb77de0453cef1d55
Josh Cohen <cohenjosh10@gmail.com>
2025-08-11 20:28:13 -0700

    fix build errors, part 2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Remove output: 'export' for Vercel deployment
-  // output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
 }
 
 module.exports = nextConfig
